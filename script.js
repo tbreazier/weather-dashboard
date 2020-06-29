6914ffde12f4cc031f39e98614b59e235d59f5e1
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-29 13:57:31 -0500

    started logic

--- script.js
+++ script.js
@@ -14,5 +14,31 @@ var city = "";
 var cityHistory = [];
 var apiKey = "36e4a7f59c83e4a1f4357162c1a5f93b";
 var iconEl = document.querySelector("img");
-var currentCity = document.querySelector("city-name");
+var currentCityEl = document.querySelector("city-name");
 var date = moment().format("MM/DD/YYYY");
+
+var formSubmitHandler = function(event) {
+    event.prevent.default();
+    var cityInput = currentCityEl.nodeValue.trim();
+};
+
+//Current Weather Function
+var getCurrentWeather = function(userCity) {
+    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
+    
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayWeather(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+//Five Day Forecast Function
+
+//Click Handler
+
+//Load History
