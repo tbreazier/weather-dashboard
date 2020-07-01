c125fca6227d5f594a40113e97b0b25c79a8744c
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-30 21:37:25 -0500

    currenweather working

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,21 +18,48 @@ var currentCityEl = document.querySelector("city-name");
 var cityFormEl = document.querySelector("city-search");
 var date = moment().format("MM/DD/YYYY");
 
-currentCityEl.textContent(city);
+// currentCityEl.text(city);
 
 //Current Weather Function
 function currentWeather(userInput) {
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey;
-
-    currentCityEl.textContent(userInput + " " + date + " ");
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey + "&units=imperial";
+    
+    // currentCityEl.text(userInput + " " + currentDate + " ");
 
     fetch(apiURL)
     .then(function(response) {
-        console.log(response);
+        response.json().then(function(data) {
+
+        function displayCurrentWeather() {
+            var iconImg = data.weather[0].icon;
+
+            var iconUrl = "http://openweathermap.org/img/w/" + iconImg + ".png";
+            iconEl.attr("src", iconUrl);
+            currentCityEl.append(iconEl);
+
+            $("#city-temp").text("Temperature: " + data.main.temp + "°F");
+            $("#city-humidity").text("Humidity: " + data.main.humidity + "%");
+            $("#city-wind-speed").text("Wind Speed: " + data.wind.speed);
+
+        }
+
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+
+        var uvApiUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&;lon=" + lon + "&APPID=" + apiKey;
+
+        displayCurrentWeather();
+
+        fetch(uvApiUrl).then(function(response) {
+            response.json().then(function(data) {
+
+            $("#city-uv").text("UV Index: " + data.value);    
+            
+            });
+        });
     });
+});
 };
-
-
 
 //Five Day Forecast Function
 
@@ -43,5 +70,3 @@ function currentWeather(userInput) {
 //Load History
 
 //Click Handler
-
-
